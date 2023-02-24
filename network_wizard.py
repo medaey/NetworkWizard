8c9cc8aa3dd1b316eb1952b9e78acfd8c402d705
medaey <medaey@hotmail.com>
2023-02-24 10:17:20 +0100

    change import module

--- network_wizard.py
+++ network_wizard.py
@@ -1,21 +1,18 @@
-import cisco.nat as nat
-import cisco.tunnel as tunnel
-import cisco.route as route
-import cisco.networks_info as networks_info
+from cisco import networks_info, nat, tunnel, route
 
 # Fonction principale
 def main ():
 	
 	# Affiche le masque et la whitecard d'un réseau par raport a la notation CIDR
-	# print(networks_info.get_netmask_for_cidr("/10"))
+	#print(networks_info.get_netmask_for_cidr("/10"))
 	
 	# Genere un script de config cisco pour du nat dynamique
-	nat.nat_dynamique_overload (
-		interfacePublic="Serial0/0/0",
-		interfacePriver="GigabitEthernet0/0",
-		ipNetPriver="172.16.0.0",
-		ipNetPriverCIDR="/24"
-	)
+	# nat.nat_dynamique_overload (
+	# 	interfacePublic="Serial0/0/0",
+	# 	interfacePriver="GigabitEthernet0/0",
+	# 	ipNetPriver="172.16.0.0",
+	# 	ipNetPriverCIDR="/24"
+	# )
 	
 	# Genere un script de config cisco cisco pour un tunnel gre
 	# tunnel.tunnel_gre (
@@ -27,11 +24,12 @@ def main ():
 	# )
 	
 	# Genere un script de config cisco de config cisco pour une route statique
-	# route.route_static (
-	# 	reseauDest = "172.16.4.0",
-	#  	reseauDestCIDR = "/22",
-	# 	ipNextRouter = "192.168.0.1"
-	# )
+
+	route.route_static (
+		reseauDest = "172.16.4.0",
+	 	reseauDestCIDR = "/22",
+		ipNextRouter = "192.168.0.1"
+	)
 
 main()
 print (f'')
